fa1aa366fc80b2ec8f1441e76e97ccb896c7285f
firazyn <noreply@github.com>
2021-01-17 00:44:35 -0800

    splash

---
+++ app/src/main/java/com/firaz/tictactoe/SplashActivity.java
@@ -0,0 +1,68 @@
+package com.firaz.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT=5000;
+    //animasi dan dekarasi
+    Animation topAnimation,bottomAnimation,middleAnimation;
+    View first,second,third,fourth,fifth,sixth;
+    TextView a,slogan;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        topAnimation= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation=AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        middleAnimation=AnimationUtils.loadAnimation(this,R.anim.middle_animation);
+
+        //inisialisasi berdasarkan id
+        first=findViewById(R.id.first_line);
+        second=findViewById(R.id.second_line);
+        third=findViewById(R.id.third_line);
+        fourth=findViewById(R.id.fourth_line);
+        fifth=findViewById(R.id.fifth_line);
+        sixth=findViewById(R.id.sixth_line);
+
+        a=findViewById(R.id.a);
+        slogan=findViewById(R.id.tagLine);
+
+
+        //animasi berdasarkan jenis
+        first.setAnimation(topAnimation);
+        second.setAnimation(topAnimation);
+        third.setAnimation(topAnimation);
+        fourth.setAnimation(topAnimation);
+        fifth.setAnimation(topAnimation);
+        sixth.setAnimation(topAnimation);
+
+        a.setAnimation(middleAnimation);
+        slogan.setAnimation(bottomAnimation);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intentmain=new Intent(SplashActivity.this,PlayGame.class);
+                startActivity(intentmain);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+}
